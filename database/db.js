f3ed705a6edd5dbd8d30c49f1c7e5d870f7bf979
Luis <lf.mc.almeida@poli.ufrj.br>
2022-06-30 17:30:31 -0300

    base project

---
+++ database/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let db;
+
+try {
+    const client = new MongoClient(process.env.DB_URL);
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("✅ Connected to the database!");
+} catch (error) {
+    console.log("❌ Database connection error", error);
+}
+
+export default db;
