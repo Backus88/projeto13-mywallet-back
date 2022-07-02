11a69f96dff7b61a484882a196a66d89046d6fca
Luis <lf.mc.almeida@poli.ufrj.br>
2022-07-02 02:58:32 -0300

    login and register middleware/schemas

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { createUser } from '../controllers/createController.js';
 import { login } from '../controllers/loginController.js';
+import validateRegister from '../middleware/validateRegister.js';
+import validateLogin from '../middleware/validateLogin.js';
 
 const authRouter = Router();
-authRouter.post('/register', createUser);
-authRouter.post('/login', login);
+authRouter.post('/register', validateRegister, createUser);
+authRouter.post('/login', validateLogin, login);
 
 export default authRouter;
---
+++ src/schemas/registerSchema.js
@@ -0,0 +1,10 @@
+import joi from 'joi';
+
+const registerSchema = joi.object({
+    name: joi.string().min(3).max(20).required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref('password')).required(),
+});
+
+export default registerSchema;
